d1e19ddd67cef23160c547a8ab9d78dbe0a1fa4f
HarukaYamamoto0 <antonioalbert344@gmail.com>
2022-11-27 17:32:59 -0300

    ajwks

--- src/main.js
+++ src/main.js
@@ -1,27 +1,26 @@
-import { Client } from "discord.js";
-import loaders from "./loaders/index.js";
-import server from "./server.js";
+import { Client } from 'discord.js'
+import loaders from './loaders/index.js'
+import server from './server.js'
 
 (async () => {
   // create the client
-  const client = new Client({ intents: 3276799 });
-
-
-
-
-
-
-
-  await loaders.loadCommands(client);
-  await loaders.loadEvents(client);
-  await server(); // starts a web server
-
-
-
-
-
-
+  const client = new Client({ intents: 3276799 })
+  
+  
+  
+  
+  
+  await loaders.loadCommands(client)
+  await loaders.loadEvents(client)
+  await server() // starts a web server
+  
+  
+  
+  
+  
+  
+  
   // login to the bot
-  client.login(process.env.tokenBot);
-  console.log("[INDEX] - index successfully loaded");
-})();
+  client.login(process.env.tokenBot)
+  console.log('[INDEX] - index successfully loaded')
+})()
